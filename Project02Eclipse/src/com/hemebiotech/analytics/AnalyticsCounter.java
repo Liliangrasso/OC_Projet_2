5fb99197674486e33bb4b0a3b53a53abab2d7345
Liliangrasso <61506049+Liliangrasso@users.noreply.github.com>
2020-04-10 13:22:54 +0200

    Rectification du code et adaptation aux besoins

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,38 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+		ISymptomReader reader = new ReadSymptomDataFromFile("D:\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt");
+		List<String> symptomList = reader.getSymptoms();
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		 Map<String,Integer> symptomMap = new TreeMap<>();
+			
+			for (String s : symptomList) {
+				if (symptomMap.get(s) == null) {
+					symptomMap.put(s,0);
+				}
+				int currentValue = symptomMap.get(s);
+				symptomMap.put(s, currentValue +1);
+			}
+		
+		
+		
+	        PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter ("result.out")));
+			
+			for (Map.Entry<String, Integer> entry : symptomMap.entrySet()) {
+				writer.print(entry.getKey() + " = " + entry.getValue() + System.getProperty("line.separator"));
+				
+			}		
 		writer.close();
-	}
 }
